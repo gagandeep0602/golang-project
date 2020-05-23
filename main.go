d087bceed055836c3282a1b3a1cd8878ed4a2230
gagandeep0602 <65246606+gagandeep0602@users.noreply.github.com>
2020-05-23 13:27:30 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -1,49 +1,98 @@
 package main
 
 import (
-	"html/template"
+	"fmt"
 	"log"
 	"net/http"
-"fmt"
- "github.com/dgraph-io/badger/v2"
+	"os"
+	"text/template"
+
+	"github.com/gorilla/websocket"
 )
 
-type NewsAggPage struct {
-	Fname    string
-	News     string
-	Lastname string
+type Display struct {
+	Rollno         int
+	Totalwords     int16
+	Totalcharacter int
+	Wordsminute    int
 }
 
-func newsAggHandler(w http.ResponseWriter, r *http.Request) {
-	p := NewsAggPage{Fname: "gagan", News: "some news", Lastname: "singh"}
-	t, _ := template.ParseFiles("basictemplating.html")
-	t.Execute(w, p)
+var y int
+
+//read and write buffer size upgrade
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 }
-func data() {
-	db, err := badger.Open(badger.DefaultOptions("/tmp/badger"))
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	// upgrade this connection to a WebSocket
+	// connection
+	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
-	err := db.Update(func(txn *badger.Txn) error {
-		err := txn.Set([]byte("answer"), []byte("42"))
-		return err
-	})
-	err := db.View(func(txn *badger.Txn) error {
-		item, err := txn.Get([]byte("answer"))
-		handle(err)
-}
-err := item.Value(func(val []byte) error {
-    // This func with val would only be called if item.Value encounters no error.
 
-    // Accessing val here is valid.
-	fmt.Printf("The answer is: %s\n", val)
+	log.Println("Client Connected")
+
+	reader(ws)
 }
+
+// ... Use conn to send and receive messages.
+
+func reader(conn *websocket.Conn) {
+	for {
+		message, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+
+			return
+		}
+
+		s := len(string(p))
+		if s > 1 {
+			g := len(string(p))
+
+			fmt.Println(g) // prints the length of the text
+
+		}
+
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(message, p); err != nil {
+
+			log.Println(err)
+			return
+		}
+
+	}
+
+}
+
+// display of teacher//
+
+func agg() {
+	p := Display{Rollno: 1,
+		Totalwords:     1,
+		Totalcharacter: y,
+		Wordsminute:    5}
+	t, _ := template.ParseFiles("basic.html")
+	t.Execute(os.Stdout, p)
+}
+func setupRoutes() {
+
+	http.HandleFunc("/ws", wsEndpoint)
+	go agg()
 }
 
 func main() {
 
-	http.HandleFunc("/", newsAggHandler)
-	http.HandleFunc("/d", data)
+	fmt.Println("Hello World")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":5500", nil))
 
-	http.ListenAndServe(":8080", nil)
 }
+
+// everything you can see in your terminal also
